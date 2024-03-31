4c1c8513f51bfc2da589adcfd33af2c75b956aa4
Sayem <sayem465508@gmail.com>
2024-03-31 14:50:58 +0600

    created a googel sign in popup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,27 @@
-
+import { GoogleAuthProvider , getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <button>Google login</button>
-        </div>
-    );
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+        const user = result.user;
+        console.log(user);
+    })
+    .catch(error => {
+        console.log('error', error.message);
+    })
+  };
+
+  console.log(app);
+  return (
+    <div>
+      <button onClick={handleGoogleSignIn}>Google login</button>
+    </div>
+  );
 };
 
 export default Login;
